eacf046327a01ee0be47f3325d3e899fb2174db0
Addynene <Addynene.chikukwaadleen@gmail.com>
2024-08-28 08:02:09 -0400

    Initial Commit

---
+++ app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<img [src]="kelele" alt="kelele">`,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'addy-Portifolio';
+ 
+}
